21b2fabda0ccba12f0e5c469c2d9af46203c1250
Dylan Morison <dylan@g.morison.org>
2021-05-24 18:08:19 -0700

    section 2 done

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
-import styles from "../styles/Layout.module.css";
+import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
 
 type props = {
 	children: any;
@@ -16,6 +18,9 @@ const Layout = ({
 	keywords = "Im lazy",
 	description = "Im lazy"
 }: props) => {
+
+	const router = useRouter()
+
 	return (
 		<div>
 			<Head>
@@ -24,6 +29,7 @@ const Layout = ({
 				<meta name="keywords" content={keywords} />
 			</Head>
 			<Header />
+			{router.pathname === '/' && <Showcase />}
 			<div className={styles.container}>{children}</div>
 			<Footer />
 		</div>
